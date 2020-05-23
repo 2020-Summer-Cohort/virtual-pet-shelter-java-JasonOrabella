8e7f4993525683325b98974caffd90f995ae21df
Jason Orabella <jasonorabella.com>
2020-05-22 23:21:49 -0400

    different approach

--- src/main/java/shelter/VirtualPet.java
+++ src/main/java/shelter/VirtualPet.java
@@ -3,17 +3,16 @@ package shelter;
 import java.util.Random;
 
 public class VirtualPet {
-
-
+    String name;
     int hunger;
     int thirst;
     int boredom;
 
-    private void virtualPet(int hunger, int thirst, int boredom) {
+    private void VirtualPet(String name,int hunger, int thirst, int boredom) {
         Random randomV = new Random();
         Random randomU = new Random();
         Random randomW = new Random();
-
+        this.name = name;
         this.thirst = randomV.nextInt(50);;
         this.hunger =  randomU.nextInt(50);
         this.boredom = randomW.nextInt(50);
--- src/main/java/shelter/VirtualPetShelter.java
+++ src/main/java/shelter/VirtualPetShelter.java
@@ -1,16 +1,24 @@
 package shelter;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 
 public class VirtualPetShelter {
+    public void VirtualPetShelter() {
+        Map<VirtualPet, Integer> = new Map<VirtualPet, Integer>();
+    }
+
+
+
+
+
+
     public void tick() {
         hunger += 10;
         boredom += 10;
         thirst += 10;
     }
-    ArrayList<String> virtualPetList = new ArrayList<String>();{
-        virtualPetList.add("Petmane");
-        virtualPetList.add(void "getNewPet)
-    }
-
 }
